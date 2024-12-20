52ebb0f9b60c53eda859ec2167623f96c73b5515
disvid <dishikavidhani@gmail.com>
2024-12-21 03:43:28 +0530

    separates tickets by their status

--- app/page.jsx
+++ app/page.jsx
@@ -8,36 +8,34 @@ const getTickets = async () => {
     });
 
     if (!res.ok) {
-      throw new Error("Failed to fetch topics");
+      throw new Error("Failed to fetch tickets");
     }
 
     return res.json();
   } catch (error) {
-    console.log("Error loading topics: ", error);
+    console.log("Error loading tickets: ", error);
   }
 };
 
 const Dashboard = async () => {
   const data = await getTickets();
 
-  
-
   const tickets = data.tickets;
 
-  const uniqueCategories = [
-    ...new Set(tickets?.map(({ category }) => category)),
+  const uniqueStatuses = [
+    ...new Set(tickets?.map(({ status }) => status)),
   ];
 
   return (
     <div className="p-5">
       <div>
         {tickets &&
-          uniqueCategories?.map((uniqueCategory, categoryIndex) => (
-            <div key={categoryIndex} className="mb-4">
-              <h2>{uniqueCategory}</h2>
+          uniqueStatuses?.map((uniqueStatus, statusIndex) => (
+            <div key={statusIndex} className="mb-4">
+              <h2>{uniqueStatus}</h2>
               <div className="lg:grid grid-cols-2 xl:grid-cols-4 ">
                 {tickets
-                  .filter((ticket) => ticket.category === uniqueCategory)
+                  .filter((ticket) => ticket.status === uniqueStatus) 
                   .map((filteredTicket, _index) => (
                     <TicketCard
                       id={_index}
@@ -54,3 +52,4 @@ const Dashboard = async () => {
 };
 
 export default Dashboard;
+
