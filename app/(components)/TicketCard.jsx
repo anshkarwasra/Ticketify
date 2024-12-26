dfceba19cf605a7788b3a8b19b76b5d1e8256522
Madhav Dhall <71399312+MadhavDhall@users.noreply.github.com>
2024-12-26 21:31:38 +0530

    Resolved issue #3 (#64) (Display ticket status)
    * Resolved issue #3
    
    * Added my name in contributors.md file
    
    ---------
    
    Co-authored-by: Madhav Dhall <generalgen06@gmail.com>

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -23,9 +23,8 @@ const TicketCard = ({ ticket }) => {
 
   return (
     <div
-      className={`flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2 ${
-        ticket.priority > 4 ? 'bg-red-500' : ''
-      }`} 
+      className={`flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2 ${ticket.status === "done" ? "bg-green-700" : ""} ${ticket.priority > 4 ? 'bg-red-500' : ''
+        } ${ticket.status === "started" ? "bg-slate-700" : ""}`}
     >
       <div className="flex mb-3">
         <PriorityDisplay priority={ticket.priority} />
@@ -34,18 +33,20 @@ const TicketCard = ({ ticket }) => {
         <h4 className="mb-1">{ticket.title}</h4>
         <hr className="h-px border-0 bg-page mb-2 " />
         <p className="whitespace-pre-wrap">{ticket.description}</p>
-  
+
         <div className="flex-grow"></div>
         <div className="flex mt-2">
           <div className="flex flex-col">
             <p className="text-xs my-1">{createdDateTime}</p>
-            <ProgressDisplay progress={ticket.progress} />
+            {ticket.status === "done" ? "" : <ProgressDisplay progress={ticket.progress} />}
           </div>
         </div>
       </Link>
+      <hr className="h-px border-0 bg-page my-2" />
+      <h6 className="mb-1 capitalize">{ticket.status}</h6>
     </div>
   );
-  
+
 };
 
 export default TicketCard;
